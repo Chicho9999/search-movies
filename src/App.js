a973de82140995b0245af7caef1706ddb5d45adf
lisandro <lisandrochichi@hotmail.com>
2020-05-04 16:13:37 -0300

    get info in API

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Title} from './Components/Title'
+import {SearchForm} from './Components/SearchForm'
+
 import './App.css';
 import 'bulma/css/bulma.css';
-import {Title} from './Components/Title'
 
-function App() {
-  return (
-    <div className="App">
-      <Title>Cosas 1</Title>
-    </div>
-  );
+class App extends Component {
+  state = {results:[]}
+  
+  _handleResults = (results) => {
+    this.setState({results})
+  }
+
+  _renderResults () {
+    const {results} = this.state
+
+    return results.map(movie => { 
+      return <p key={movie.imdbID}>{movie.Title}</p>
+      }
+    )
+
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Title>Cosas 1</Title>
+        <div className="SearchForm-wrapper"> 
+          <SearchForm onResults={this._handleResults} />
+        </div>
+        {!this.state.results.length
+          ? <p>No results</p>
+          : this._renderResults()}
+      </div>
+    );
+  }
+  
 }
 
 export default App;
